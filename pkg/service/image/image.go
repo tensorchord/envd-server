4a874e2bf32f8d430977e1cf0a3b482af618b14e
Ce Gao <cegao@tensorchord.ai>
2022-12-15 10:46:44 +0800

    fix(image): Remove duplicate images (#128)
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

---
+++ pkg/service/image/image.go
@@ -0,0 +1,125 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+package image
+
+import (
+	"context"
+	"net/url"
+
+	"github.com/cockroachdb/errors"
+	"github.com/jackc/pgtype"
+	"github.com/jackc/pgx/v4"
+
+	"github.com/tensorchord/envd-server/api/types"
+	"github.com/tensorchord/envd-server/errdefs"
+	"github.com/tensorchord/envd-server/pkg/query"
+)
+
+type Service interface {
+	CreateImageIfNotExist(ctx context.Context,
+		owner, name string) (*types.ImageMeta, error)
+	ListImages(ctx context.Context,
+		owner string) ([]types.ImageMeta, error)
+	GetImage(ctx context.Context,
+		owner, name string) (*types.ImageMeta, error)
+}
+
+type generalService struct {
+	querier query.Querier
+}
+
+type CreateOptions struct {
+	OwnerToken string `json:"owner_token"`
+	Name       string `json:"name"`
+	Digest     string `json:"digest"`
+	Created    int64  `json:"created"`
+	Size       int64  `json:"size"`
+}
+
+func NewService(querier query.Querier) Service {
+	return &generalService{querier: querier}
+}
+
+func (g generalService) CreateImageIfNotExist(ctx context.Context,
+	owner, name string) (*types.ImageMeta, error) {
+	meta, err := g.GetImage(ctx, owner, name)
+	if err != nil {
+		if errdefs.IsNotFound(err) {
+			// Create a new image
+			meta, err = g.createImage(ctx, owner, name)
+			if err != nil {
+				return nil, errors.Wrap(err, "g.createImage")
+			}
+			return meta, nil
+		} else {
+			return nil, errors.Wrap(err, "g.GetImage")
+		}
+	}
+	return meta, nil
+}
+
+func (g generalService) createImage(ctx context.Context,
+	owner, name string) (*types.ImageMeta, error) {
+	meta, err := g.fetchMetadata(ctx, name)
+	if err != nil {
+		return nil, errors.Wrap(err, "fetch metadata")
+	}
+	var pglabel pgtype.JSONB
+	err = pglabel.Set(meta.Labels)
+	if err != nil {
+		return nil, errors.Wrap(err, "pglabel.set")
+	}
+
+	_, err = g.querier.CreateImageInfo(context.Background(),
+		query.CreateImageInfoParams{OwnerToken: owner,
+			Name: meta.Name, Digest: meta.Digest,
+			Created: meta.Created, Size: meta.Size, Labels: pglabel})
+	return &meta, err
+}
+
+func (g generalService) ListImages(ctx context.Context,
+	owner string) ([]types.ImageMeta, error) {
+	images, err := g.querier.ListImageByOwner(context.Background(), owner)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			// No image found
+			return nil, nil
+		} else {
+			return nil, errors.Wrap(err, "failed to list image by owner")
+		}
+	}
+	res := []types.ImageMeta{}
+	for _, info := range images {
+		item, err := daoToImageMeta(info)
+		if err != nil {
+			return nil, errors.Wrap(err, "daoToImageMeta")
+		}
+		res = append(res, *item)
+	}
+	return res, nil
+}
+
+func (g generalService) GetImage(ctx context.Context,
+	owner, name string) (*types.ImageMeta, error) {
+	name, err := url.PathUnescape(name)
+	if err != nil {
+		return nil, errors.Wrap(err, "url.PathUnescape")
+	}
+
+	imageInfo, err := g.querier.GetImageInfo(context.Background(), query.GetImageInfoParams{OwnerToken: owner, Name: name})
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, errdefs.NotFound(err)
+		} else {
+			return nil, errors.Wrap(err, "g.querier.GetImageInfo")
+		}
+	}
+	meta, err := daoToImageMeta(imageInfo)
+	if err != nil {
+		return nil, errors.Wrap(err, "util.DaoToImageMeta")
+	}
+
+	return meta, nil
+}
--- pkg/service/image/util.go
+++ pkg/service/image/util.go
@@ -2,14 +2,14 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-package util
+package image
 
 import (
 	"github.com/tensorchord/envd-server/api/types"
 	"github.com/tensorchord/envd-server/pkg/query"
 )
 
-func DaoToImageMeta(dao query.ImageInfo) (*types.ImageMeta, error) {
+func daoToImageMeta(dao query.ImageInfo) (*types.ImageMeta, error) {
 	var label map[string]string
 	if err := dao.Labels.AssignTo(&label); err != nil {
 		return nil, err
