9ce0625eed6fbde43cdd1ff4349034f205732735
Ce Gao <cegao@tensorchord.ai>
2023-01-17 12:12:07 +0800

    feat: Support resource quota limit
    Signed-off-by: Ce Gao <cegao@tensorchord.ai>

--- pkg/runtime/kubernetes/kubernetes.go
+++ pkg/runtime/kubernetes/kubernetes.go
@@ -15,22 +15,25 @@ type generalProvisioner struct {
 	client kubernetes.Interface
 	logger *logrus.Entry
 
-	namespace           string
-	imagePullSecretName *string
+	namespace            string
+	imagePullSecretName  *string
+	resourceQuotaEnabled bool
 }
 
 func NewProvisioner(client kubernetes.Interface,
-	namespace, imagePullSecretName string) runtime.Provisioner {
+	namespace, imagePullSecretName string, resourceQuotaEnabled bool) runtime.Provisioner {
 	p := &generalProvisioner{
 		client:    client,
 		namespace: namespace,
 		logger: logrus.WithFields(logrus.Fields{
 			"namespace":              namespace,
 			"image-pull-secret-name": imagePullSecretName,
+			"resource-quota-enabled": resourceQuotaEnabled,
 		}),
 	}
 	if imagePullSecretName != "" {
 		p.imagePullSecretName = &imagePullSecretName
 	}
+	p.resourceQuotaEnabled = resourceQuotaEnabled
 	return p
 }
